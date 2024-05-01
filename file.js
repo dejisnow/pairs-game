5b06b209ab069f2f623238c1f165468b18f6a138
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-05-01 22:16:52 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,27 @@
+window.onload=()=>{
 
+let allBoxes = document.querySelectorAll(".box")
+
+ let boxOne = document.querySelector(".box.one");
+ let boxTwo = document.querySelector(".box.two"); 
+ let boxThree= document.querySelector(".box.three");
+ let boxFour= document.querySelector(".box.four");
+ let boxFive = document.querySelector(".box.five");
+let boxSix = document.querySelector(".box.six");
+   
+   
+   allBoxes.forEach((box)=>{
+       box.addEventListener("click",()=>{
+         box.a
+           
+       })
+   })
+   
+   
+   
+   
+    
+    
+    
+    
+}
